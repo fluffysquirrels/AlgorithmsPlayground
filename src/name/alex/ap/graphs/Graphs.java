9cea73762d2dc682b552be12f83d5dbba47600c1
Alex <alex@plato>
2014-04-29 22:27:33 +0100

    BFS implementation but with no tests, so it probably doesn't work.

--- src/name/alex/ap/graphs/Graphs.java
+++ src/name/alex/ap/graphs/Graphs.java
@@ -3,8 +3,10 @@ package name.alex.ap.graphs;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 public class Graphs {
     private Graphs(){}
@@ -112,5 +114,41 @@ public class Graphs {
         );
 
         return foundCycle != null && foundCycle == true;
+    }
+    
+    public static void breadthFirstSearch(UndirectedGraph g) {
+        HashMap<Integer, VertexVisitedState> visitedVertexMap = new HashMap<>();
+        HashSet<UndirectedEdge> traversedEdgeSet = new HashSet<>();
+        Queue<Integer> toVisit = new LinkedList<>();
+        
+        for(Vertex v: g.getVertices()) {
+            breadthFirstSearchRecursive(
+                    g, visitedVertexMap, traversedEdgeSet,
+                    v.getIndex());
+        }
+    }
+
+    private static void breadthFirstSearchRecursive(
+            UndirectedGraph g, HashMap<Integer, VertexVisitedState> visitedVertexMap,
+            HashSet<UndirectedEdge> traversedEdgeSet,
+            Integer currVertexIndex) {
+
+        VertexVisitedState vertexState = visitedVertexMap.get(currVertexIndex);
+
+        if(vertexState == null){
+            vertexState = VertexVisitedState.NotVisited;
+        }
+
+        visitedVertexMap.put(currVertexIndex, VertexVisitedState.BeingTraversed);
+        for(UndirectedEdge outEdge: g.getEdgesFromVertex(currVertexIndex)) {
+            if(traversedEdgeSet.contains(outEdge)){
+                continue;
+            }
+            traversedEdgeSet.add(outEdge);
+
+            breadthFirstSearchRecursive(g, visitedVertexMap,
+                    traversedEdgeSet, outEdge.getTo());
+        }
+        visitedVertexMap.put(currVertexIndex, VertexVisitedState.BeenTraversed);
     }
 }
